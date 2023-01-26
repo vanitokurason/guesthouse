977572725335cc64f634ffdb71c02bc3e6d5b2f1
vanito-kurason <vanitocurason@gmail.com>
2023-01-26 18:25:02 +0300

    first commit

---
+++ config/connection.php
@@ -0,0 +1,5 @@
+<?php
+const DB_USER = 'Kurason';
+const DB_PASS = 'Kiparis_88';
+const DB_NAME = 'Guesthouse';
+const DB_CHARSET = 'utf-8';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vanitokurason/orderlist',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'web-application',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vanitokurason/orderlist' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'web-application',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
